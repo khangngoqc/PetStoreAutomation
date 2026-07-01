3fb0d63d8686f355bc8fc2c4b9d6ca5e1c6f685d
khangngo <nnd.khangvn@gmail.com>
2026-07-01 22:17:42 +0700

    First commit

---
+++ src/test/java/api/endpoints/Routes.java
@@ -0,0 +1,22 @@
+package api.endpoints;
+
+
+
+public class Routes {
+	
+	public static String base_url = "petstore.swagger.io/v2";
+	
+	//User module
+	public static String post_url= base_url + "/user";
+	public static String get_url= base_url + "/user/{username}";
+	public static String update_url= base_url + "/user/{username}";
+	public static String delete_url= base_url + "/user/{username}";
+	
+	
+	//Store module
+		//add Store module URLs
+	
+	//Pet module
+		//add Pet module URLs
+		
+}
---
+++ src/test/java/api/test/UserTests.java
@@ -0,0 +1,44 @@
+package api.test;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.github.javafaker.Faker;
+
+import api.endpoints.UserEndPoints;
+import api.payload.User;
+import io.restassured.response.Response;
+
+public class UserTests {
+	
+	Faker faker;
+	User userPayload;
+	
+	@BeforeClass
+	public void setupData() {
+		
+		faker = new Faker();
+		userPayload = new User();
+		userPayload.setId(faker.number().randomDigit());
+		userPayload.setUsername(faker.name().username());
+		userPayload.setFirstname(faker.name().firstName());
+		userPayload.setLastname(faker.name().lastName());
+		userPayload.setEmail(faker.internet().emailAddress());
+		userPayload.setPassword(faker.internet().password());
+		userPayload.setPhone(faker.phoneNumber().phoneNumber());
+		
+	}
+	
+	@Test
+	public void testPostUser() {
+		
+		Response res = UserEndPoints.createUser(userPayload);
+		res.then().log().all();
+		
+		Assert.assertEquals(res.getStatusCode(), 200);
+	}
+	
+
+}
+
